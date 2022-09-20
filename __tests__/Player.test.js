e18fe4a629c3f29037f12d77315fe0e768c5260e
kabradshaw1 <itllbok82@gmail.com>
2022-09-20 12:20:37 -0400

    10.3.3

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,6 +4,12 @@ jest.mock('../lib/Potion');
 
 const Player = require('../lib/Player');
 
+test("gets player's health value", () => {
+  const player = new Player('Dave');
+
+  expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
 test('creates a player object', () => {
   const player = new Player('Dave');
 
@@ -25,4 +31,27 @@ test('creates a player object', () => {
 //   expect(player.inventory).toEqual(
 //     expect.arrayContaining([expect.any(Object)])
 //   );
+});
+
+test('checks if player is alive or not', () => {
+  const player = new Player('Dave');
+
+  expect(player.isAlive()).toBeTruthy();
+
+  player.health = 0;
+
+  expect(player.isAlive()).toBeFalsy();
+});
+
+test("subtracts from player's health", () => {
+  const player = new Player('Dave');
+  const oldHealth = player.health;
+
+  player.reduceHealth(5);
+
+  expect(player.health).toBe(oldHealth - 5);
+
+  player.reduceHealth(99999);
+
+  expect(player.health).toBe(0);
 });
